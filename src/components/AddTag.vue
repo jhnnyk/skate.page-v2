63deddbd9c11c1ab9801300191cfadd67272a991
John Knotts <jhnnyk@gmail.com>
2024-07-09 14:34:40 -0600

    show hide add tags form

--- src/components/AddTag.vue
+++ src/components/AddTag.vue
@@ -5,10 +5,16 @@ const newTag = defineModel('newTag')
 
 <template>
   <div>
-    <button v-if="!showForm" @click="showForm = true">add tags</button>
+    <button
+      v-if="!showForm"
+      @click="showForm = true"
+      class="text-slate-50 bg-sky-700 px-2 pb-0.5 rounded-full"
+    >
+      +
+    </button>
     <form v-if="showForm">
-      <h4>Add a tag</h4>
       <input type="text" id="tag" placeholder="New tag" v-model="newTag" />
+      <button @click="showForm = false" class="text-xs pl-2">close</button>
     </form>
   </div>
 </template>
