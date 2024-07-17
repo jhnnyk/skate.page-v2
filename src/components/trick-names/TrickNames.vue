06087ae2a75e766fcec063f56f9d0bf42bcd9edf
John Knotts <jhnnyk@gmail.com>
2024-07-17 12:29:49 -0600

    alphabetize trick names

--- src/components/trick-names/TrickNames.vue
+++ src/components/trick-names/TrickNames.vue
@@ -2,6 +2,7 @@
 import AddName from '@/components/trick-names/AddName.vue'
 import getUser from '@/composables/getUser'
 import useDocument from '@/composables/useDocument'
+import { computed, watch } from 'vue'
 
 const props = defineProps({ trick: Object })
 const { user } = getUser()
@@ -11,12 +12,29 @@ const handleDeleteName = async (name) => {
   const newNameList = props.trick.names.filter((n) => n.name != name)
   await updateDocument({ names: newNameList })
 }
+
+const sortedNames = computed(() => {
+  const toBeSorted = props.trick.names
+  return toBeSorted.sort((a, b) => {
+    const nameA = a.name.toUpperCase() // ignore upper and lowercase
+    const nameB = b.name.toUpperCase() // ignore upper and lowercase
+    if (nameA < nameB) {
+      return -1
+    }
+    if (nameA > nameB) {
+      return 1
+    }
+
+    // names must be equal
+    return 0
+  })
+})
 </script>
 
 <template>
   <div class="grow p-2 rounded-md bg-slate-200">
     <h4 class="font-bold">Names</h4>
-    <ul v-for="name in trick.names" :key="name" class="list-disc ml-4">
+    <ul v-for="name in sortedNames" :key="name" class="list-disc ml-4">
       <li>
         <div class="flex">
           {{ name.name }}
